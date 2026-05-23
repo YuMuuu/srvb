fe72b1f4ab0ac06e779b1be9c273c9cf3b615454
yu miura <curelemonade15@gmail.com>
2026-05-24 03:11:56 +0900

    update to jucev8 and rm choc

--- native/WebViewEditor.h
+++ native/WebViewEditor.h
@@ -3,11 +3,9 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_gui_extra/juce_gui_extra.h>
 
-#include <choc_WebView.h>
-
 
 //==============================================================================
-// A simple juce::AudioProcessorEditor that holds a choc::WebView and sets the
+// A simple juce::AudioProcessorEditor that holds a JUCE WebBrowserComponent and sets the
 // WebView instance to cover the entire region of the editor.
 class WebViewEditor : public juce::AudioProcessorEditor
 {
@@ -16,7 +14,7 @@ public:
     WebViewEditor(juce::AudioProcessor* proc, juce::File const& assetDirectory, int width, int height);
 
     //==============================================================================
-    choc::ui::WebView* getWebViewPtr();
+    juce::WebBrowserComponent* getWebViewPtr();
 
     //==============================================================================
     void paint (juce::Graphics& g) override;
@@ -24,16 +22,11 @@ public:
 
 private:
     //==============================================================================
-    choc::value::Value handleSetParameterValueEvent(const choc::value::ValueView& e);
+    std::optional<juce::WebBrowserComponent::Resource> getResource(const juce::String& path) const;
+    void handleNativeMessage(const juce::var& args);
+    void handleSetParameterValueEvent(const juce::var& e);
 
     //==============================================================================
-    std::unique_ptr<choc::ui::WebView> webView;
-
-#if JUCE_MAC
-    juce::NSViewComponent viewContainer;
-#elif JUCE_WINDOWS
-    juce::HWNDComponent viewContainer;
-#else
- #error "We only support MacOS and Windows here yet."
-#endif
+    juce::File assetDirectory;
+    std::unique_ptr<juce::WebBrowserComponent> webView;
 };
